64bf9879fc0964971bee80f0aa5ba095da277df3
Ashrafi Ar Raad <letmkdir@gmail.com>
2021-02-04 20:28:19 +0600

    All files added succesfully

---
+++ js/main.js
@@ -0,0 +1,9 @@
+console.log("Gi");
+console.log("afd");
+console.log("ag");
+console.log("ah");
+console.log("ah");
+console.log("ah");
+console.log("gh");
+console.log("jf");
+console.log("Raad Sarker");
