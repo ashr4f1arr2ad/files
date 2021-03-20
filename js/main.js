88b12fcd04218f6902dc11278c678cd19f55a4a1
Ashrafi Arraad <44719020+ashrafiarraad@users.noreply.github.com>
2021-03-21 01:14:07 +0600

    All page design complete

--- js/main.js
+++ js/main.js
@@ -42,6 +42,33 @@
         controls: ['play', 'progress', 'current-time'],
     });
 
+    /*------------------
+        CountDown
+    --------------------*/
+    //For demo preview
+    var today = new Date();
+    var dd = String(today.getDate()).padStart(2, '0');
+    var mm = String(today.getMonth() + 1).padStart(2, '0'); //January is 0!
+    var yyyy = today.getFullYear();
 
+    if (mm == 12) {
+        mm == '01';
+        yyyy == yyyy + 1;
+    } else {
+        mm = parseInt(mm) + 1;
+        mm = String(mm).padStart(2, '0');
+    }
+
+    var timerdate = mm + '/' + dd + '/' + yyyy;
+
+    console.log(timerdate);
+
+    //Use this for real time date
+    $("#countdown").countdown("2021/3/30", function (event) {
+        $(this).html(event.strftime("<div class='cd-item'><span>%D</span> <p>Days</p> </div>" + "<div class='cd-item'><span>%H</span> <p>Hours</p> </div>" + "<div class='cd-item'><span>%M</span> <p>Minutes</p> </div>" + "<div class='cd-item'><span>%S</span> <p>Seconds</p> </div>"));
+    });
+
+    $( "#datepicker" ).datepicker();
+    $( "#datepicker2" ).datepicker();
 
 })(jQuery);
